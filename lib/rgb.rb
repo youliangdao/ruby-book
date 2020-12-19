0f356a27ec830926776e16af3912101c0abd9894
youliangdao <youliangdaojing5@gmail.com>
2020-12-19 12:45:30 +0900

    RGBプログラムの作成2

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -4,6 +4,13 @@ def to_hex(r, g, b)
   end
 end
 
-def to_ints(s)
-  [0, 0, 0]
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  ints = []
+  [r, g, b].each do |s|
+    ints << s.hex
+  end
+  ints
 end
--- spec/rgb_spec.rb
+++ spec/rgb_spec.rb
@@ -8,7 +8,9 @@ RSpec.describe "RGB" do
       expect(to_hex(12, 38, 49)).to eq "#0c2631"
     end
     it "正しい配列を返すこと" do
-      expect(to_ints("000000")).to eq [0, 0, 0]
+      expect(to_ints("#000000")).to eq [0, 0, 0]
+      expect(to_ints("#ffffff")).to eq [255, 255, 255]
+      expect(to_ints("#903cd8")).to eq [144, 60, 216]
     end
 end
 
