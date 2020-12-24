a045923c1d3caa3bd6870da8c5168c51af7623a9
youliangdao <youliangdaojing5@gmail.com>
2020-12-24 14:01:03 +0900

    正しいロジックを実装

--- spec/convert_length_spec.rb
+++ spec/convert_length_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "ConvertLength" do
     it "正しい数値を返すこと" do
       expect(convert_length(1, 'm', 'in')).to eq 39.37
       expect(convert_length(15, 'in', 'm')).to eq 0.38
+      expect(convert_length(35000, 'ft', 'm')).to eq 10670.73
     end
 end
 
