678fc285ad811bdc2d95d2242175b5786f14d9b5
youliangdao <youliangdaojing5@gmail.com>
2020-12-24 13:51:23 +0900

    仮実装

--- spec/convert_length_spec.rb
+++ spec/convert_length_spec.rb
@@ -1,8 +1,9 @@
 require_relative '../spec/spec_helper.rb'
+require_relative '../lib/convert_length.rb'
 
 RSpec.describe "ConvertLength" do
     it "正しい数値を返すこと" do
-      expect(convert_length(1, 'm', 'in')).to eq "39.37"
+      expect(convert_length(1, 'm', 'in')).to eq 39.37
     end
 end
 
